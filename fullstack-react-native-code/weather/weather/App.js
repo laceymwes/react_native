17421f2c00822e0c2deb92942bb4d44dffe1d70a
Michael Lacey <laceymwes@gmail.com>
2018-09-30 12:51:49 -0600

    Component styling

---
+++ fullstack-react-native-code/weather/weather/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  KeyboardAvoidingView,
+  Platform,
+  TextInput,
+} from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={[styles.largeText, styles.textStyle]}>San Francisco</Text>
+        <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text>
+        <Text style={[styles.largeText, styles.textStyle]}>24</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textStyle: {
+    textAlign: 'center',
+    fontFamily: Platform.os === 'ios' ? 'AvenirNext-Regular' : 'Roboto'
+  },
+  largeText: {
+    fontSize: 44,
+  },
+  smallText: {
+    fontSize: 18,
+  },
+});
